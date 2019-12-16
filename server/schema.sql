b7a1cee0f9b66ce312ddbad5b3fb4dff159814bb
Joy <greeniejoy@gmail.com>
2019-12-16 11:51:55 -0600

    changed schema

--- server/schema.sql
+++ server/schema.sql
@@ -12,12 +12,22 @@ CREATE TABLE questions
 (
   id int NOT NULL
   AUTO_INCREMENT,
-  catagory varchar
-  (50) NOT NULL,
   question varchar
   (255) NOT NULL,
-  answer varchar
+  rightAnswer varchar
   (100) NOT NULL,
+  wrongAnswer1 varchar
+  (100) Not Null,
+  wrongAnswer2 varchar
+  (100) ,
+  wrongAnswer3 varchar
+  (100) Not Null,
+  wrongAnswer4 varchar
+  (100),
+  rightCount int
+  (25),
+   wrongCount int
+  (25),
   PRIMARY KEY
   (id)
 );
