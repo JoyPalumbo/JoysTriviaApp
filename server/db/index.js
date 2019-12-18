ce7b85a1e370f288962761f40ee344a4fda8a843
Joy <greeniejoy@gmail.com>
2019-12-18 00:22:38 -0600

    we deploying or what

--- server/db/index.js
+++ server/db/index.js
@@ -2,13 +2,13 @@ const mysql = require('mysql');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const {
-  DB_USER,
-  DB_PASSWORD,
-  DB_HOST,
-  DB_PORT,
-  DB_DATABASE,
-} = process.env;
+// const {
+//   DB_USER,
+//   DB_PASSWORD,
+//   DB_HOST,
+//   DB_PORT,
+//   DB_DATABASE,
+// } = process.env;
 
 // const DB_HOST = 'localhost';
 // const DB_USER = 'root';
